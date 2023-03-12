12469f16d5f2fa42e5ece19566cb84106bc323c0
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-12 14:50:08 +0300

    HW Task 2_1

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3-х значное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int num(int num)
+{
+  int num1 = num % 100;
+  int num2 = num1 / 10;
+  return num2;
+}
+
+int result = num(a);
+
+Console.WriteLine(result);
