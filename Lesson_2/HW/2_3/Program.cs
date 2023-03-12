2a915a7590906e86e1ed757b853b5c2a2fba8ca0
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-12 20:15:30 +0300

    HW Task 2_3

---
+++ Lesson_2/HW/2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трехзначное
+// число и удаляет вторую цифру этого числа.
+// Пример:
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int TakeNum(int num)
+{
+  Console.WriteLine(num);
+  int num1 = num / 100;
+  int num2 = num % 10;
+  return (num1 * 10 + num2);
+}
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
