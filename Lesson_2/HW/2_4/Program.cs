1ebb9bedc80e4cb4f51ea46662e6f1deca3c1db3
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-13 18:56:36 +0300

    HW Task 2_4

--- Lesson_2/HW/2_4/Program.cs
+++ Lesson_2/HW/2_4/Program.cs
@@ -38,3 +38,29 @@ else if (day == 7)
 }
 else
   Console.WriteLine("Данное число не относиться к дням недели!");
+
+/*
+Второе решение:
+
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine()!);
+string[] weeks =
+{
+  "Понедельник - Рабочий день!",
+  "Вторник - Рабочий день!",
+  "Среда - Рабочий день!",
+  "Четверг - Рабочий день!",
+  "Пятница - Рабочий день!",
+  "Суббота - Выходной день!",
+  "Воскресенье - Выходной день!"
+};
+if (day == 1) Console.WriteLine(weeks[0]);
+if (day == 2) Console.WriteLine(weeks[1]);
+if (day == 3) Console.WriteLine(weeks[2]);
+if (day == 4) Console.WriteLine(weeks[3]);
+if (day == 5) Console.WriteLine(weeks[4]);
+if (day == 6) Console.WriteLine(weeks[5]);
+if (day == 7) Console.WriteLine(weeks[6]);
+else
+  Console.WriteLine("Данное число не относиться к дням недели!");
+*/
