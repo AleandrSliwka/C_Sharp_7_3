ff6aaa2395911dfe89b605c6a1f785a1ba9f7667
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-20 13:07:16 +0300

    HW Task 3_3

---
+++ Lesson_3/HW/3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая принимает на вход 
+число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void S(int n)
+{
+  int a = 1;
+  while (a <= n)
+  {
+    Console.WriteLine(a * a * a);
+    a++;
+  }
+}
+Console.WriteLine("Ввидите число: ");
+S(int.Parse(Console.ReadLine()!));
