8a6b98aed2f75697765bc1698a391acbbad7bbd8
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-21 18:39:24 +0300

    HW Task 1_2

---
+++ Lesson_4/HW/1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// 3. Напишите программу, которая задаёт массив из 8 элементов
+//    случайными числами и выводит их на экран. Оформите заполнение массива
+//    и вывод в виде функции (пригодится в следующих задачах)
+
+
+void Print(int[] arr)
+{
+  int size = arr.Length;
+  for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
+  Console.WriteLine();
+}
+
+int[] RandMass(int size, int from, int to)
+{
+  int[] arr = new int[size];
+
+  for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(from, to + 1);
+  return arr;
+}
+
+Console.Write("Enter array length: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter minimal range value: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter maximum range value: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = RandMass(num, start, stop);
+Print(mass);
