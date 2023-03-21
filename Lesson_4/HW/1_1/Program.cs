28a2a5071973216778ad11afabf9956d3deb60cb
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-21 18:01:00 +0300

    HW Task 1_1

---
+++ Lesson_4/HW/1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 27: Напишите программу, которая принимает 
+              на вход число и выдаёт сумму цифр в числе.
+              Пример:
+              452 -> 11
+              82 -> 10
+              9012 -> 12
+*/
+
+Console.WriteLine("Write A number: ");
+int a = int.Parse(Console.ReadLine()!);
+int SumNum(int num)
+{
+  int num_1 = 0;
+  while (num > 0)
+  {
+    num_1 += num % 10;
+    num /= 10;
+  }
+  return num_1;
+}
+SumNum(a);
+Console.WriteLine($"Сумма чисел заданного числа = {SumNum(a)}");
