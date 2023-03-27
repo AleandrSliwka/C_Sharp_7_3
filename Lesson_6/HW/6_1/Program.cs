8b3797fe4abc39342c0d1d779921280d904f14a1
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-27 16:06:46 +0300

    HW Task 6_1

---
+++ Lesson_6/HW/6_1/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 3: Напишите программу, которая будет создавать 
+//           копию заданного двумерного массива с помощью 
+//           поэлементного копирования.
+
+
+void Print(int[,] arr)
+{
+  int size = arr.Length;
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void Print1(int[,] arr)
+{
+  int size = arr.Length;
+
+  for (int i1 = 0; i1 < arr.GetLength(0); i1++)
+  {
+    for (int j1 = 0; j1 < arr.GetLength(1); j1++)
+    {
+      Console.Write($"{arr[i1, j1]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] RandMass(int size, int size1, int from, int to)
+{
+  int[,] arr = new int[size, size1];
+  //int[,] copy = arr.Clone() as int[,];
+  int[,] copy = new int[size, size1];
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(from, to + 1);
+    }
+  }
+  return arr;
+
+  for (int i1 = 0; i1 < copy.GetLength(0); i1++)
+  {
+    for (int j1 = 0; j1 < copy.GetLength(1); j1++)
+    {
+      Console.Write($"{copy[i1, j1]}");
+    }
+    Console.WriteLine();
+  }
+  return copy;
+}
+
+
+Console.Write("Enter the number of lines: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter minimal range value: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter maximum range value: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = RandMass(num, num1, start, stop);
+Print(mass);
+Console.WriteLine();
+Console.WriteLine("Copy Mass:");
+Console.WriteLine();
+Print1(mass);
