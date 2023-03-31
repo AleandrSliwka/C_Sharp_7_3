9e34f13db4b0a0b4a806422c5b54e75e7ad93a1f
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-03-31 15:50:55 +0300

    HW Lesson_7

--- Lesson_6/HW/6_3/Program.cs
+++ Lesson_6/HW/6_3/Program.cs
@@ -2,7 +2,7 @@
 //           пересечения двух прямых, заданных уравнениями 
 //           y = k1 * x + b1, y = k2 * x + b2; 
 //           значения b1, k1, b2 и k2 задаются пользователем.
-https://studok.net/obrazovanie-i-kommunikacii/vopros-kak-vychislit-tochku-peresechenija-dvuh-prjamyh/
+// https://studok.net/obrazovanie-i-kommunikacii/vopros-kak-vychislit-tochku-peresechenija-dvuh-prjamyh/
 //           Пример:
 //           b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
