50bfb39115b0c1e708864012b388e9120951b856
Alexandr Slivka <alexandr.sliwka@yandex.ru>
2023-04-05 14:57:07 +0300

    Lesson_9

---
+++ Lesson_9/HW/9_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+//           которая выведет все чётные натуральные числа 
+//           в промежутке от M до N с помощью рекурсии.
+//           M = 1; N = 5 -> "2, 4"
+//           M = 4; N = 8 -> "4, 6, 8"
+
+Console.WriteLine("Write a number M: ");
+int firstValue = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write a number N: ");
+int secondValue = int.Parse(Console.ReadLine()!);
+
+void evenNaturalNumber(int M, int N)
+{
+  if (M > N) return;
+  if (N % 2 == 0)
+  {
+    evenNaturalNumber(M, N - 2);
+    Console.Write($"{N} ");
+  }
+  if (N % 2 != 0)
+  {
+    N = N - 1;
+    evenNaturalNumber(M, N - 2);
+    Console.Write($"{N} ");
+  }
+}
+evenNaturalNumber(firstValue, secondValue);
---
+++ Lesson_9/HW/9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+//           которая найдёт сумму натуральных элементов 
+//           в промежутке от M до N с помощью рекурсии.
+
+//           M = 1; N = 15 -> 120
+//           M = 4; N = 8 -> 30
+
+Console.WriteLine("Write a number M: ");
+int firstValue = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Write a number N: ");
+int secondValue = int.Parse(Console.ReadLine()!);
+
+int evenNaturalNumber(int M, int N)
+{
+  if (M > N) return 0;
+  return evenNaturalNumber(M, N - 1) + N;
+}
+
+Console.WriteLine(evenNaturalNumber(firstValue, secondValue));
